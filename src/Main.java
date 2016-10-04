e81890f41a154303df6abecb1be324c087038d94
Mattias Svensson <matte_sesso@hotmail.com>
2016-10-04 13:57:45 +0200

    Second commit. change in sumDouble method with local variabel

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		public int sumDouble(int a, int b) {
-			  if (a == b){
-			    return (a + b)*2;
+			int sum = a + b;  
+			
+			if (a == b){
+			    return sum *2;
 			  }
 			  else 
-			    return a+b;
+			    return sum;
 			}
 
 	}
