475f4d91a2018f35e3bf4d00bd3e67778c25d2e4
Mattias Svensson <matte_sesso@hotmail.com>
2016-10-04 13:51:37 +0200

    My first commit. Started with a little method to math of two numbers

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		public int sumDouble(int a, int b) {
+			  if (a == b){
+			    return (a + b)*2;
+			  }
+			  else 
+			    return a+b;
+			}
+
+	}
+
+}
